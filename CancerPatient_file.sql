3d806d83ef93c79343fc9dd7535795a6f28de99a
Nishi Sen <nishi.sen@exazeit.com>
2025-02-05 12:51:00 +0530

    Add files via upload

---
+++ CancerPatient_file.sql
@@ -0,0 +1,23 @@
+create database cancer
+
+use  cancer
+
+select * from cancer_patientData
+
+
+select count([Patient Id])  from cancer_patientData
+where Gender=2 and Level='High'
+
+select count([Patient Id])  from cancer_patientData
+where Gender=1 and Level='High'
+
+
+select [Alcohol use],smoking,[Dust Allergy]
+   from cancer_patientData
+   where [Alcohol use]=3 and [Dust Allergy]=9
+
+
+   
+select COUNT([Patient Id])
+   from cancer_patientData
+   where =3
